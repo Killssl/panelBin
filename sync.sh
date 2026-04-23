0afc842625ae2676ac594cd1597a557b8bc26497
Kiselyov Vladimir <kiselyovvld@mail.ru>
2026-04-23 18:26:36 +0300

    update sync.sh for ggoduu user

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avz -e "ssh" --exclude ".git" --exclude ".idea" --exclude "__pycache__" --exclude "data" . aezakmi@187.77.111.121:~/repo/panelbin
+rsync -avz -e "ssh" --exclude ".git" --exclude ".idea" --exclude "__pycache__" --exclude "data" . ggoduu@187.77.111.121:~/repo/panelbin
